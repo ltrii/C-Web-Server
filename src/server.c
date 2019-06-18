ba1eeffd1c310d621bf9b976c8b0072e6a4c4886
ltrii <lelandtr@gmail.com>
2019-06-18 13:15:17 -0700

    Additional work

--- src/server.c
+++ src/server.c
@@ -132,9 +132,30 @@ void resp_404(int fd)
  */
 void get_file(int fd, struct cache *cache, char *request_path)
 {
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+    char filepath[262144];
+    struct file_data *filedata;
+    char *mime_type;
+
+    // Fetch file
+    sprintf(filepath, "%s"
+                      "%s",
+            SERVER_ROOT, request_path);
+
+    filedata = file_load(filepath);
+
+    if (filedata == NULL)
+    {
+        resp_404(fd);
+    }
+    else
+    {
+
+        mime_type = mime_type_get(filepath);
+
+        send_response(fd, "HTTP/1.1 200 OK", mime_type, filedata->data, filedata->size);
+
+        file_free(filedata);
+    }
 }
 
 /**
